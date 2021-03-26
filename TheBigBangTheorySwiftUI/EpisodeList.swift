d06181ca3d32568f4e6a13380dee8b98cadc1cc4
Jorge <jorgesantamariapoveda@gmail.com>
2021-03-26 23:22:49 +0100

    Diseño listado episodios

--- TheBigBangTheorySwiftUI/EpisodeList.swift
+++ TheBigBangTheorySwiftUI/EpisodeList.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct EpisodeList: View {
+    @State var visto = false
+    @State var favorito = false
+    @State var puntuacion: CGFloat = 1
+
     var body: some View {
-        Text("Episode list")
+        List {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Nombre")
+                    Toggle("Visto", isOn: $visto)
+                    Toggle("Favorito", isOn: $favorito)
+                    Slider(value: $puntuacion,
+                           in: 1...5,
+                           minimumValueLabel: Text("1"),
+                           maximumValueLabel: Text("5"),
+                           label: { Text("Espaciado") })
+                }
+                Spacer()
+                Image(systemName: "person")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .padding(.horizontal)
+            }
+        }
     }
 }
 
