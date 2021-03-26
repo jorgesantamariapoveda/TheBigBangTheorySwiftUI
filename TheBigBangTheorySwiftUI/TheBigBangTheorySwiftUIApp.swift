fbea5a5ce0e503cd369a3fd79b763e93a4a2238e
Jorge <jorgesantamariapoveda@gmail.com>
2021-03-26 20:59:16 +0100

    Initial Commit

---
+++ TheBigBangTheorySwiftUI/TheBigBangTheorySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheBigBangTheorySwiftUIApp.swift
+//  TheBigBangTheorySwiftUI
+//
+//  Created by Jorge on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TheBigBangTheorySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
